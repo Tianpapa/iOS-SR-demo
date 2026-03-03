5306202a4d2d52304092aea08c9b280f41f4b8ed
Irene <fu_ailin@dahuatech.com>
2026-03-03 14:30:56 +0800

    InferenceModule初步完成MNN推理模块的代码引入

--- SuperResolutionDemo/Inference/InferenceModule.mm
+++ SuperResolutionDemo/Inference/InferenceModule.mm
@@ -6,6 +6,68 @@
 
 #import "InferenceModule.h"
 #import <Libtorch-Lite/Libtorch-Lite.h>
+#import <Metal/Metal.h>
+
+#import <AVFoundation/AVFoundation.h>
+#import <MNN/HalideRuntime.h>
+#import <MNN/MNNDefine.h>
+#import <MNN/ErrorCode.hpp>
+#import <MNN/ImageProcess.hpp>
+#import <MNN/Interpreter.hpp>
+#import <MNN/Tensor.hpp>
+#define MNN_METAL
+#import <MNN/MNNSharedContext.h>
+#import <MNN/expr/Module.hpp>
+#import <MNN/expr/Executor.hpp>
+#import <MNN/expr/ExprCreator.hpp>
+#import <MNN/AutoTime.hpp>
+
+struct PretreatInfo {
+    int outputSize[4];
+    float mean[4];
+    float normal[4];
+    float inputSize[4];
+    float matrix[16];
+};
+
+struct GpuCache {
+    CVMetalTextureCacheRef _textureCache;
+    id<MTLDevice> _device;
+    id<MTLComputePipelineState> _pretreat;
+    id<MTLFunction> _function;
+    id<MTLBuffer> _constant;
+    id<MTLCommandQueue> _queue;
+    GpuCache() {
+        _device = MTLCreateSystemDefaultDevice();
+        CVReturn res = CVMetalTextureCacheCreate(nil, nil, _device, nil, &_textureCache);
+        FUNC_PRINT(res);
+        id<MTLLibrary> library = [_device newDefaultLibrary];
+        _function = [library newFunctionWithName:@"pretreat"];
+        NSError* error = nil;
+        _pretreat = [_device newComputePipelineStateWithFunction:_function error:&error];
+        _constant = [_device newBufferWithLength:sizeof(PretreatInfo) options:MTLCPUCacheModeDefaultCache];
+        _queue = [_device newCommandQueue];
+    }
+    ~ GpuCache() {
+        
+    }
+};
+
+@interface Model : NSObject {
+    std::shared_ptr<MNN::Express::Executor::RuntimeManager> _rtmgr;
+    std::shared_ptr<MNN::Express::Module> _module;
+    std::shared_ptr<MNN::Interpreter> _net;
+    MNN::Session *_session;
+    std::mutex _mutex;
+    MNNForwardType _type;
+    MNN::Tensor* _input;
+    MNN::Tensor* _output;
+    std::shared_ptr<GpuCache> _cache;
+}
+@property (strong, nonatomic) UIImage *defaultImage;
+@property (strong, nonatomic) NSArray<NSString *> *labels;
+
+@end
 
 // 640x640 is the default image size used in the export.py in the yolov5 repo to export the TorchScript model, 25200*85 is the model output size
 // const int input_width = 640;
